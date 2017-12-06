4458f417c48afd3e5511d61ee32d4ac8aec14acf
Paul Sonnentag <paul.sonnentag@googlemail.com>
2017-12-05 21:51:03 -0800

    Import references into mongodb

---
+++ importer/importer.js
@@ -0,0 +1,217 @@
+const _ = require('lodash/fp')
+const util = require('util')
+const fs = require('fs-extra')
+const path = require('path')
+const glob = util.promisify(require('glob'))
+const exec = util.promisify(require('child_process').exec)
+const git = require('simple-git/promise')
+const compareVersions = require('compare-versions')
+
+const ELM_VERSION = '0.18.0'
+
+const MAIN_FILE_REGEX =
+  new RegExp(
+    _.map(file => `(${file}$)`, [
+      'main.elm',
+      'index.elm',
+      'app.elm',
+    ])
+      .join('|')
+    ,
+    'gi'
+  )
+
+async function importRepo(context) {
+  const { rootDir, user, repoName } = context
+
+  const repo = await fetchRepo(context)
+  const latestCommit = await getLatestCommit(repo)
+  const package = await getPackage(context)
+
+  const dependencies = await resolveDependencies(context)
+
+  /*
+  console.log('latest commit:', latestCommit)
+  console.log('package:', package)
+  console.log('dependencies:', dependencies)
+  */
+
+  const references = _.map(
+    reference => {
+      return Object.assign(reference, {
+        version: dependencies[`${reference.user}/${reference.project}`],
+        commit: latestCommit.hash
+      })
+    },
+    await getReferences(context)
+  )
+
+  return {
+    hash: latestCommit.hash,
+    references
+  }
+}
+
+async function fetchRepo({ user, repoName, rootDir }) {
+  const workingDir = getWorkingDir({ user, repoName, rootDir })
+  let repo
+
+  if (await fs.pathExists(path.join(workingDir, '.git'))) {
+    repo = git(workingDir)
+    await repo.pull()
+
+  } else {
+    await fs.ensureDir(workingDir)
+    repo = git(workingDir)
+    await repo.clone(`https://github.com/${user}/${repoName}.git`, '.')
+  }
+
+  return repo
+}
+
+const VERSION_REGEX = /^([0-9]+\.[0-9]+\.[0-9]+) <= v < ([0-9]+\.[0-9]+\.[0-9]+)$/
+
+async function getPackage({ user, repoName, rootDir }) {
+  const workingDir = getWorkingDir({ user, repoName, rootDir })
+
+  let package
+
+  try {
+    package = await fs.readJson(path.join(workingDir, 'elm-package.json'))
+  } catch (err) {
+    if (err.code == 'ENOENT') {
+      throw new ImporterError(`repository doens't have "elm-package.json"`)
+    }
+
+    throw err
+  }
+
+  const version = package['elm-version']
+  const match = version.match(VERSION_REGEX)
+
+  if (!match) {
+    throw new ImporterError(`couldn't parse elm-version: "${version}"`)
+  }
+
+  const [, minVersion, maxVersion] = match
+
+  // compareVersion: a < b => -1; a == b => 0; a > b => 1
+  if (compareVersions(ELM_VERSION, minVersion) == -1 || compareVersions(ELM_VERSION, maxVersion) >= 0) {
+    throw new ImporterError(`current version "${ELM_VERSION}" doesn't match required version "${version}"`)
+  }
+
+  return package
+}
+
+async function getLatestCommit(repo) {
+  return (await repo.log()).latest
+}
+
+async function resolveDependencies({ user, repoName, rootDir }) {
+  const workingDir = getWorkingDir({ user, repoName, rootDir })
+
+  // install packages
+  await execWithDefaultErrorHandler(`lib/install.sh ${workingDir}`)
+
+  return await fs.readJson(path.join(workingDir, 'elm-stuff/exact-dependencies.json'))
+}
+
+async function getReferences({ user, repoName, rootDir }) {
+  const workingDir = getWorkingDir({ user, repoName, rootDir })
+  const paths = await glob(path.join(workingDir, '**/*.elm'))
+  const files = _.flow(
+    _.map(path => _.drop(workingDir.length, path).join('')),
+    _.reject(_.startsWith('/elm-stuff'))
+  )(paths)
+  const [mainFiles, regularFiles] = _.partition(file => MAIN_FILE_REGEX.test(file), files)
+
+  let queue = mainFiles.concat(regularFiles)
+  let references = []
+
+  // clear build artifacts from previous build
+  await fs.emptyDir(path.join(workingDir, 'elm-stuff/build-artifacts'))
+
+  while (!_.isEmpty(queue)) {
+    let current = _.first(queue)
+
+    let newReferences = await compileFile(workingDir, _.first(queue))
+    let resolvedFiles = _.reduce((map, { file }) => Object.assign(map, { [file]: true }), {}, newReferences)
+
+    references = references.concat(newReferences)
+    queue = queue.filter(file => !resolvedFiles[file] && file !== current)
+  }
+
+  return references
+}
+
+async function compileFile(workingDir, file) {
+  const filePath = path.join(workingDir, file)
+  const lines = (await execWithDefaultErrorHandler(`lib/make.sh ${workingDir} ${filePath}`)).split('\n')
+
+  const VALUE_REGEX = /External value [(`](.*)[`)] exists!!Canonical \{_package = Name \{_user = "(.*)", _project = "(.*)"}, _module = "(.*)"}/m
+
+  const log =
+    _.flow(
+      _.flatMap(line => {
+        let obj = []
+        try { obj = JSON.parse(line) } catch (e) { }
+        return obj
+      }),
+      _.map(message => {
+        if (message.tag !== 'external value') {
+          return null
+        }
+
+        const match = message.overview.replace('\n', ' ').match(VALUE_REGEX)
+
+        if (!match) {
+          console.warn(`couldn't parse message: "${message.overview}"`)
+          return null
+        }
+
+        const [,symbol, user, project, module] = match
+
+        // ignore project internal references for now
+        // TODO: handle local references
+        if (user === 'user' && project === 'project') {
+          return null
+        }
+
+        return {
+          user, project, module,
+          symbol: symbol,
+          region: message.region,
+          file: message.file.slice(1),
+        }
+      }),
+      _.compact
+    )(lines)
+
+  return log
+}
+
+async function execWithDefaultErrorHandler(cmd) {
+  let { stdout, stderr } = await exec(cmd, { maxBuffer: 1024 * 1000 })
+
+  if (stderr) {
+    throw new Error(stderr)
+  }
+
+  return stdout
+}
+
+function getWorkingDir({ user, repoName, rootDir }) {
+  return path.join(__dirname, `${rootDir}/${user}/${repoName}`)
+}
+
+class ImporterError extends Error {
+  constructor(...args) {
+    super(...args)
+    Error.captureStackTrace(this, ImporterError)
+  }
+}
+
+module.exports = {
+  ImporterError,
+  importRepo
+}
--- importer/index.js
+++ importer/index.js
@@ -1,227 +1,42 @@
 const _ = require('lodash/fp')
-const util = require('util')
-const fs = require('fs-extra')
-const path = require('path')
-const glob = util.promisify(require('glob'))
-const exec = util.promisify(require('child_process').exec)
-const git = require('simple-git/promise')
-const compareVersions = require('compare-versions')
+const MongoClient = require('mongodb').MongoClient
+const {importRepo, ImporterError} = require('./importer')
 
-const ELM_VERSION = '0.18.0'
+const DB_URL = 'mongodb://localhost:27017/elm'
 
-const MAIN_FILE_REGEX =
-  new RegExp(
-    _.map(file => `(${file}$)`, [
-      'main.elm',
-      'index.elm',
-      'app.elm',
-    ])
-      .join('|')
-    ,
-    'gi'
-  )
-
-importRepo({
-  rootDir: '_repos',
-  user: 'evancz',
-  repoName: 'start-app'
-})
-
-async function importRepo(context) {
-  const { rootDir, user, repoName } = context
-
+async function run () {
 
   try {
-    console.log(`import ${user}/${repoName}`)
+    const db = await MongoClient.connect(DB_URL)
+    const col = await db.collection('references')
 
-    const repo = await fetchRepo(context)
-    const latestCommit = await getLatestCommit(repo)
-    const package = await getPackage(context)
+    console.log('start import')
 
-    const dependencies = await resolveDependencies(context)
+    const {references, hash} = await importRepo({
+      rootDir: '_repos',
+      user: 'rtfeldman',
+      repoName: 'elm-spa-example'
+    })
 
-    console.log('latest commit:', latestCommit)
-    console.log('package:', package)
-    console.log('dependencies:', dependencies)
+    console.log('find and remove', { commit: hash })
 
-    const symbols = await getReferences(context)
+    // clear previous values
+    await col.deleteMany({ commit: hash })
 
-    console.log('symbols:', symbols)
+    // insert new references
+    await col.insertMany(references)
+
+    console.log(`imported ${references.length} reference(s)`)
+
+    db.close()
 
   } catch (err) {
-    if (!(err instanceof ImporterError)) {
+    if (err instanceof ImporterError) {
+      console.log('Importer Error:', err.message)
+    } else {
       throw err
     }
-
-    console.log('Failed:', err.message)
   }
 }
 
-async function fetchRepo({ user, repoName, rootDir }) {
-  const workingDir = getWorkingDir({ user, repoName, rootDir })
-  let repo
-
-  if (await fs.pathExists(path.join(workingDir, '.git'))) {
-    console.log('pull')
-    repo = git(workingDir)
-    // await repo.pull()
-
-  } else {
-    console.log('clone')
-    await fs.ensureDir(workingDir)
-    repo = git(workingDir)
-    await repo.clone(`https://github.com/${user}/${repoName}.git`, '.')
-  }
-
-  return repo
-}
-
-const VERSION_REGEX = /^([0-9]+\.[0-9]+\.[0-9]+) <= v < ([0-9]+\.[0-9]+\.[0-9]+)$/
-
-async function getPackage({ user, repoName, rootDir }) {
-  const workingDir = getWorkingDir({ user, repoName, rootDir })
-
-  let package
-
-  try {
-    package = await fs.readJson(path.join(workingDir, 'elm-package.json'))
-  } catch (err) {
-    if (err.code == 'ENOENT') {
-      throw new ImporterError(`repository doens't have "elm-package.json"`)
-    }
-
-    throw err
-  }
-
-  const version = package['elm-version']
-  const match = version.match(VERSION_REGEX)
-
-  if (!match) {
-    throw new ImporterError(`couldn't parse elm-version: "${version}"`)
-  }
-
-  const [, minVersion, maxVersion] = match
-
-  // compareVersion: a < b => -1; a == b => 0; a > b => 1
-  if (compareVersions(ELM_VERSION, minVersion) == -1 || compareVersions(ELM_VERSION, maxVersion) >= 0) {
-    throw new ImporterError(`current version "${ELM_VERSION}" doesn't match required version "${version}"`)
-  }
-
-  console.log('version is fine!', ELM_VERSION, minVersion, maxVersion)
-
-  return package
-}
-
-async function getLatestCommit(repo) {
-  return (await repo.log()).latest
-}
-
-async function resolveDependencies({ user, repoName, rootDir }) {
-  const workingDir = getWorkingDir({ user, repoName, rootDir })
-
-  // install packages
-  await execWithDefaultErrorHandler(`lib/install.sh ${workingDir}`)
-
-  return await fs.readJson(path.join(workingDir, 'elm-stuff/exact-dependencies.json'))
-}
-
-async function getReferences({ user, repoName, rootDir }) {
-  const workingDir = getWorkingDir({ user, repoName, rootDir })
-  const paths = await glob(path.join(workingDir, '**/*.elm'))
-  const files = _.flow(
-    _.map(path => _.drop(workingDir.length, path).join('')),
-    _.reject(_.startsWith('/elm-stuff'))
-  )(paths)
-
-  console.log(files)
-
-  const [mainFiles, regularFiles] = _.partition(file => MAIN_FILE_REGEX.test(file), files)
-
-  let queue = mainFiles.concat(regularFiles)
-  let references = []
-
-  // clear build artifacts from previous build
-  await fs.emptyDir(path.join(workingDir, 'elm-stuff/build-artifacts'))
-
-  console.log('total files:', queue.length)
-
-  while (!_.isEmpty(queue)) {
-    let current = _.first(queue)
-
-    let newReferences = await compileFile(workingDir, _.first(queue))
-    let resolvedFiles = _.reduce((map, { file }) => Object.assign(map, { [file]: true }), {}, newReferences)
-
-    references = references.concat(newReferences)
-    queue = queue.filter(file => !resolvedFiles[file] && file !== current)
-
-    console.log('compiled', current, 'with dependen files: ', _.keys(resolvedFiles).length, ' references: ', newReferences.length)
-  }
-
-  return references
-}
-
-async function compileFile(workingDir, file) {
-  const filePath = path.join(workingDir, file)
-  const lines = (await execWithDefaultErrorHandler(`lib/make.sh ${workingDir} ${filePath}`)).split('\n')
-
-  const VALUE_REGEX = /External value [(`](.*)[`)] exists!!Canonical \{_package = Name \{_user = "(.*)", _project = "(.*)"}, _module = "(.*)"}/m
-
-  const log =
-    _.flow(
-      _.flatMap(line => {
-        let obj = []
-        try { obj = JSON.parse(line) } catch (e) { }
-        return obj
-      }),
-      _.map(message => {
-        if (message.tag !== 'external value') {
-          return null
-        }
-
-        const match = message.overview.replace('\n', ' ').match(VALUE_REGEX)
-
-        if (!match) {
-          console.warn(`couldn't parse message: "${message.overview}"`)
-          return null
-        }
-
-        const [symbol, user, project, module] = match
-
-        return {
-          user, project, module,
-          symbol: symbol,
-          region: message.region,
-          file: message.file.slice(1),
-        }
-      }),
-      _.compact
-    )(lines)
-
-  return log
-}
-
-async function execWithDefaultErrorHandler(cmd) {
-  let { stdout, stderr } = await exec(cmd, { maxBuffer: 1024 * 1000 })
-
-  if (stderr) {
-    throw new Error(stderr)
-  }
-
-  return stdout
-}
-
-function getWorkingDir({ user, repoName, rootDir }) {
-  return path.join(__dirname, `${rootDir}/${user}/${repoName}`)
-}
-
-class ImporterError extends Error {
-  constructor(...args) {
-    super(...args)
-    Error.captureStackTrace(this, ImporterError)
-  }
-}
-
-module.exports = {
-  ImporterError,
-  importRepo
-}
+run()
