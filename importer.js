bb2c9dd5c0d20c6621d26189415ecbc2d75281c5
Paul Sonnentag <paul.sonnentag@googlemail.com>
2018-06-04 12:58:13 +0200

    Fetch all elm repos from github

--- importer.js
+++ importer.js
@@ -21,8 +21,10 @@ const MAIN_FILE_REGEX =
     'gi'
   )
 
-async function importRepo(context) {
-  const { user, repoName } = context
+const REPO_DIR = '_repos'
+
+async function importRepo (context) {
+  const {owner, name} = context
 
   const repo = await fetchRepo(context)
   const latestCommit = await getLatestCommit(repo)
@@ -30,17 +32,17 @@ async function importRepo(context) {
 
   return await Promise.all(_.map(
     async reference => {
-      const filePath = !reference.file.startsWith('/') ? '/' + reference.file : path.join(__dirname, '_repos', user, repoName, reference.file)
+      const filePath = !reference.file.startsWith('/') ? '/' + reference.file : path.join(__dirname, REPO_DIR, owner, name, reference.file)
       const module = await getModuleOfFile(filePath)
 
       const startLine = reference.region.start.line
       const endLine = reference.region.end.line
-      const lineSelector = startLine === endLine ? `L${startLine}` : `L${startLine}:${endLine}`;
-      const url = `https://github.com/${user}/${repoName}/blob/${latestCommit.hash}${reference.file}#${lineSelector}`;
+      const lineSelector = startLine === endLine ? `L${startLine}` : `L${startLine}:${endLine}`
+      const url = `https://github.com/${owner}/${name}/blob/${latestCommit.hash}${reference.file}#${lineSelector}`
 
       return Object.assign(reference, {
         referer: {
-          project: `${user}/${repoName}`,
+          project: `${owner}/${name}`,
           module,
         },
 
@@ -59,11 +61,11 @@ async function importRepo(context) {
       })
     },
     await getReferences(context)
-  ));
+  ))
 }
 
-async function fetchRepo({ user, repoName, rootDir }) {
-  const workingDir = getWorkingDir({ user, repoName, rootDir })
+async function fetchRepo ({owner, name}) {
+  const workingDir = getWorkingDir({owner, name})
   let repo
 
   if (await fs.pathExists(path.join(workingDir, '.git'))) {
@@ -73,7 +75,7 @@ async function fetchRepo({ user, repoName, rootDir }) {
   } else {
     await fs.ensureDir(workingDir)
     repo = git(workingDir)
-    await repo.clone(`https://github.com/${user}/${repoName}.git`, '.')
+    await repo.clone(`https://github.com/${owner}/${name}.git`, '.')
   }
 
   return repo
@@ -81,19 +83,23 @@ async function fetchRepo({ user, repoName, rootDir }) {
 
 const VERSION_REGEX = /^([0-9]+\.[0-9]+\.[0-9]+) <= v < ([0-9]+\.[0-9]+\.[0-9]+)$/
 
-async function getPackage({ user, repoName, rootDir }) {
-  const workingDir = getWorkingDir({ user, repoName, rootDir })
+async function getPackage ({owner, name, rootDir}) {
+  const workingDir = getWorkingDir({owner, name, rootDir})
 
   let package
 
   try {
     package = await fs.readJson(path.join(workingDir, 'elm-package.json'))
   } catch (err) {
-    if (err.code == 'ENOENT') {
-      throw new ImporterError(`repository doesn't have "elm-package.json"`)
-    }
+    try {
+      package = await fs.readJson(path.join(workingDir, 'elm.json'))
+    } catch (err) {
+      if (err.code == 'ENOENT') {
+        throw new ImporterError(`repository doesn't have "elm-package.json"`)
+      }
 
-    throw err
+      throw err
+    }
   }
 
   const version = package['elm-version']
@@ -113,12 +119,12 @@ async function getPackage({ user, repoName, rootDir }) {
   return package
 }
 
-async function getLatestCommit(repo) {
+async function getLatestCommit (repo) {
   return (await repo.log()).latest
 }
 
-async function resolveDependencies({ user, repoName, rootDir }) {
-  const workingDir = getWorkingDir({ user, repoName, rootDir })
+async function resolveDependencies ({owner, name, rootDir}) {
+  const workingDir = getWorkingDir({owner, name, rootDir})
 
   // install packages
   await execWithDefaultErrorHandler(`lib/install.sh ${workingDir}`)
@@ -133,14 +139,14 @@ const getModuleOfFile = _.memoize(async path => {
 
   if (!match) {
     console.warn('Couldn\'t get module of file: ', path)
-    return null;
+    return null
   }
 
-  return match[1];
+  return match[1]
 })
 
-async function getReferences({ user, repoName, rootDir }) {
-  const workingDir = getWorkingDir({ user, repoName, rootDir })
+async function getReferences ({owner, name, rootDir}) {
+  const workingDir = getWorkingDir({owner, name, rootDir})
   const paths = await glob(path.join(workingDir, '**/*.elm'))
   const files = _.flow(
     _.map(path => _.drop(workingDir.length, path).join('')),
@@ -157,15 +163,19 @@ async function getReferences({ user, repoName, rootDir }) {
   while (!_.isEmpty(queue)) {
     let currentFileName = _.first(queue)
 
-    let resolvedFiles = { [currentFileName]: true };
+    let resolvedFiles = {[currentFileName]: true}
 
     try {
       let newReferences = await compileFile(workingDir, _.first(queue))
-      resolvedFiles = _.reduce((map, { file }) => Object.assign(map, { [file]: true }), resolvedFiles, newReferences)
+      resolvedFiles = _.reduce((map, {file}) => Object.assign(map, {[file]: true}), resolvedFiles, newReferences)
       references = references.concat(newReferences)
 
+      console.log(`compile ${currentFileName}: success`)
+
     } catch (e) {
-      console.warn(`failed to compile ${currentFileName}`)
+      console.log(e);
+
+      console.warn(`compile ${currentFileName}: failed`)
     }
 
     queue = queue.filter(file => !resolvedFiles[file])
@@ -174,53 +184,50 @@ async function getReferences({ user, repoName, rootDir }) {
   return references
 }
 
-async function compileFile(workingDir, file) {
+async function compileFile (workingDir, file) {
   const filePath = path.join(workingDir, file)
   const lines = (await execWithDefaultErrorHandler(`lib/make.sh ${workingDir} ${filePath}`)).split('\n')
   const VALUE_REGEX = /External value [(`](.*)[`)] exists!!Canonical \{_package = Name \{_user = "(.*)", _project = "(.*)"}, _module = "(.*)"}/m
 
-  const log =
-    _.flow(
-      _.flatMap(line => {
-        let obj = []
-        try { obj = JSON.parse(line) } catch (e) { }
-        return obj
-      }),
-      _.map(message => {
-        if (message.tag !== 'external value') {
-          return null
-        }
+  return _.flow(
+    _.flatMap(line => {
+      let obj = []
+      try { obj = JSON.parse(line) } catch (e) { }
+      return obj
+    }),
+    _.map(message => {
+      if (message.tag !== 'external value') {
+        return null
+      }
 
-        const match = message.overview.replace('\n', ' ').match(VALUE_REGEX)
+      const match = message.overview.replace('\n', ' ').match(VALUE_REGEX)
 
-        if (!match) {
-          console.warn(`couldn't parse message: "${message.overview}"`)
-          return null
-        }
+      if (!match) {
+        console.warn(`couldn't parse message: "${message.overview}"`)
+        return null
+      }
 
-        const [,symbol, user, project, module] = match
+      const [, symbol, user, project, module] = match
 
-        // ignore project internal references for now
-        // TODO: handle local references
-        if (user === 'user' && project === 'project') {
-          return null
-        }
+      // ignore project internal references for now
+      // TODO: handle local references
+      if (user === 'user' && project === 'project') {
+        return null
+      }
 
-        return {
-          user, project, module,
-          symbol: symbol,
-          region: message.region,
-          file: message.file.slice(1),
-        }
-      }),
-      _.compact
-    )(lines)
-
-  return log
+      return {
+        user, project, module,
+        symbol: symbol,
+        region: message.region,
+        file: message.file.slice(1),
+      }
+    }),
+    _.compact
+  )(lines)
 }
 
-async function execWithDefaultErrorHandler(cmd) {
-  let { stdout, stderr } = await exec(cmd, { maxBuffer: 1024 * 1000 })
+async function execWithDefaultErrorHandler (cmd) {
+  let {stdout, stderr} = await exec(cmd, {maxBuffer: 1024 * 1000})
 
   if (stderr) {
     throw new Error(stderr)
@@ -229,12 +236,12 @@ async function execWithDefaultErrorHandler(cmd) {
   return stdout
 }
 
-function getWorkingDir({ user, repoName, rootDir }) {
-  return path.join(__dirname, `${rootDir}/${user}/${repoName}`)
+function getWorkingDir ({owner, name}) {
+  return path.join(__dirname, REPO_DIR, owner, name)
 }
 
 class ImporterError extends Error {
-  constructor(...args) {
+  constructor (...args) {
     super(...args)
     Error.captureStackTrace(this, ImporterError)
   }
--- index.js
+++ index.js
@@ -1,46 +1,99 @@
 const _ = require('lodash/fp')
 const {importRepo, ImporterError} = require('./importer')
 const fs = require('fs-extra')
-
+const axios = require('axios')
 const neo4j = require('neo4j-driver').v1
 
 const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'password'))
 const session = driver.session()
 
+const PAGE_SIZE = 10
+
 ;(async () => {
   try {
+    let currentPage = 1
 
-    await loadRepo({
-      rootDir: '_repos',
-      user: 'rtfeldman',
-      repoName: 'elm-spa-example'
-    })
+    do {
+      const response = await axios.get(`https://api.github.com/search/repositories?q=language:elm&per_page=${PAGE_SIZE}&page=${currentPage}`)
+      const repos = response.data.items
 
-    await session.close()
+      totalResults = response.data.total_count
+      currentPage++
 
+      for (let i = 0; i < repos.length; i++) {
+        const repo = repos[i];
+
+        try {
+          await loadRepo({
+            stars: repo.stargazers_count,
+            owner: repo.owner.login,
+            name: repo.name,
+            lastUpdated: repo.updated_at,
+            license: repo.license.key
+          })
+        } catch (err) {
+          console.error('Failed import, Unexpected Error:', err)
+        }
+      }
+    } while (((currentPage - 1) * PAGE_SIZE) < totalResults)
+
+  } catch (err) {
+    console.error(err)
+  }
+
+  process.exit();
+
+})()
+
+async function loadRepo (repo) {
+  const {owner, stars, lastUpdated, license, name} = repo
+  const timestamp = Date.now()
+  console.log(`${owner}/${name} start import`)
+
+  let references = []
+
+  try {
+    await importRepo({owner, name})
   } catch (err) {
     if (err instanceof ImporterError) {
       console.error('Importer Error:', err.message)
     } else {
-      console.error('Unexpected Error:', err)
+      throw err
     }
   }
-})()
 
-async function loadRepo ({rootDir, user, repoName}) {
-  console.log(`${user}/${repoName} start import`)
+  await fs.writeFile(`_references/${owner}_${name}.json`, JSON.stringify(references, null, 2))
 
-  const references = await importRepo({rootDir, user, repoName})
-
-  await fs.writeFile(`_references/${user}_${repoName}.json`, JSON.stringify(references, null, 2))
+  await addRepoMetaDataToGraph(repo)
 
   await addReferencesToGraph(references)
 
-  console.log(`${user}/${repoName} successfully imported ${references.length} reference(s)`)
+  const duration = Math.round((Date.now() - timestamp) / 1000)
+
+  console.log(`${owner}/${name} successfully imported ${references.length} reference(s) in ${duration} second(s)`)
+}
+
+async function addRepoMetaDataToGraph ({owner, stars, lastUpdated, license, name}) {
+  await session.run(`
+    MERGE
+      (project:Project {id: $project})
+    ON MATCH SET
+      p.lastUpdated = $lastUpdated,
+      p.license = $license,
+      p.starts = $stars
+    ON CREATE SET
+      p.lastUpdated = $lastUpdated,
+      p.license = $license,
+      p.starts = $stars
+  `, {
+    project: `${owner}/${name}`,
+    lastUpdated,
+    license,
+    stars
+  })
 }
 
 async function addReferencesToGraph (references) {
-
   // ensure referenced projects, modules and symbols exist
   await Promise.all(_.flow(
     _.flatMap(({referer, referred, symbol}) => [
